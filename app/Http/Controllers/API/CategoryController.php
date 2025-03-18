b664d3140df0c77110ba6d133513d23b248a1207
protocol <oujaberousama@gmail.com>
2025-03-18 14:36:04 +0000

    ha hia aba aziz

---
+++ app/Http/Controllers/API/CategoryController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $category = Category::all();
+
+        return response()->json([
+            'data' => $category
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'category_name' => 'required',
+        ]);
+
+        $category = Category::create($request->all());
+
+        return response()->json([
+            'msg' => 'success',
+            'category' => $category
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $category = Category::find($id);
+
+        if (!$category) {
+            return response()->json([
+                'msg' => 'not found'
+            ]);
+        }
+
+        return response()->json([
+            'category' => $category
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $category = Category::find($id);
+
+        if (!$category) {
+            return response()->json([
+                'msg' => 'category not found'
+            ]);
+        }
+
+        $category->delete();
+
+        return response()->json([
+            'msg' => 'deleted successfully'
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $category = Category::find($id);
+
+        if (!$category) {
+            return response()->json([
+                'msg' => 'shop rise!'
+            ]);
+        }
+    }
+}
